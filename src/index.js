810f7be63937ef995fb05b32afded067866a2780
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-02 14:09:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,27 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+
+  // const toyCollection = document.querySelector("#toy-collection");
+  //
+  // fetch('http://localhost:3000/toys')
+  //   .then(response => response.json())
+  //   .then(object => {
+  //     console.log(object)
+  //
+  //     for (element of object) {
+  //       let cardDiv = document.createElement('div')
+  //       cardDiv.classList.add('card')
+  //       addInfoToCard(cardDiv);
+  //     }
+  //   })
+  //
+  //   function addInfoToCard(card) {
+  //     card.innerHTML = `<h2>Woody</h2>
+  //     <img src=toy_image_url class="toy-avatar" />
+  //     <p>4 Likes </p>
+  //     <button class="like-btn">Like <3</button>`
+  //   }
+
+
 });
