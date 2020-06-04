bbfe62e9378dacab24742572dc55e501332b63e8
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-04 12:36:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,26 +14,64 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // const toyCollection = document.querySelector("#toy-collection");
-  //
-  // fetch('http://localhost:3000/toys')
-  //   .then(response => response.json())
-  //   .then(object => {
-  //     console.log(object)
-  //
-  //     for (element of object) {
-  //       let cardDiv = document.createElement('div')
-  //       cardDiv.classList.add('card')
-  //       addInfoToCard(cardDiv);
-  //     }
-  //   })
-  //
-  //   function addInfoToCard(card) {
-  //     card.innerHTML = `<h2>Woody</h2>
-  //     <img src=toy_image_url class="toy-avatar" />
-  //     <p>4 Likes </p>
-  //     <button class="like-btn">Like <3</button>`
-  //   }
+  const toyNameInput = document.getElementsByClassName('input-text')[0]
+  const toyImgInput = document.getElementsByClassName('input-text')[1]
+  const createToyBtn = document.getElementsByClassName('submit')[0]
+  const toyCollection = document.querySelector("#toy-collection");
 
+  function fetchDatafromDb() {
+    fetch('http://localhost:3000/toys')
+      .then(response => response.json())
+      .then(array => array.forEach(element => renderToys(element)))
+  }
 
-});
+  function renderToys(element) {
+    toyCollection.insertAdjacentHTML('beforeend',
+      `<div class="card">
+          <h2>${element.name}</h2>
+          <img src=${element.image} class="toy-avatar" />
+          <p class="likes">${element.likes} Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div>`
+    )
+    const likeBtns = document.querySelectorAll('.like-btn')
+    likeBtns.forEach(btn => btn.addEventListener('click', increaseLikes))
+    
+    function increaseLikes() {
+      let configObj = {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+          'Accept': "application/json"
+        },
+        body: JSON.stringify({
+          "likes": element.likes + 1
+        })
+      }
+      fetch(`http://localhost:3000/toys/${element.id}`, configObj)
+    }
+  }
+
+  createToyBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    let dataToPost = {
+      "name": toyNameInput.value,
+      "image": toyImgInput.value,
+      "likes": 0
+    }
+    let configObj = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        'Accept': "application/json"
+      },
+      body: JSON.stringify(dataToPost)
+    }
+    fetch('http://localhost:3000/toys', configObj)
+      .then(response => response.json())
+      .then(object => renderToys(object))
+  })
+
+  fetchDatafromDb();
+
+})
